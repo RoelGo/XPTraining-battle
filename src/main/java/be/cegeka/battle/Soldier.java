644bdf4d2ee6a66e6feb279b3adde03f2cc7e15d
Roel Goossens <roelgoossens22@hotmail.com>
2017-05-05 13:38:33 +0200

    Added a weapon to Soldier

--- src/main/java/be/cegeka/battle/Soldier.java
+++ src/main/java/be/cegeka/battle/Soldier.java
@@ -6,15 +6,22 @@ import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 public class Soldier {
 
+
+
     private String name;
+    private Weapon weapon;
 
     public Soldier(String name) {
         Validate.isTrue(isNotBlank(name));
-
+        this.weapon = Weapon.BAREFIST;
         this.name = name;
     }
 
     String getName() {
         return this.name;
+    }
+
+    public Weapon getWeapon() {
+        return weapon;
     }
 }
---
+++ src/main/java/be/cegeka/battle/Weapon.java
@@ -0,0 +1,11 @@
+package be.cegeka.battle;
+
+/**
+ * Created by roel on 05.05.17.
+ */
+public enum Weapon {
+    AXE,
+    SWORD,
+    SPEAR,
+    BAREFIST
+}
