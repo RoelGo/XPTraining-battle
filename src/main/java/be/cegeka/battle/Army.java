95f3096ae41b81ef8c82dd173002a2cc1f858590
Christoph <christoph.marckx@cegeka.com>
2017-05-05 14:17:00 +0200

    Portuguese and fashion clone Portuguese wannabe: Story 4 finished

---
+++ src/main/java/be/cegeka/battle/Army.java
@@ -0,0 +1,25 @@
+package be.cegeka.battle;
+
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
+
+/**
+ * Created by christom on 5/05/2017.
+ */
+public class Army {
+
+    private List<Soldier> troops = newArrayList();
+
+    public List<Soldier> getTroops() {
+        return troops;
+    }
+
+    public void addTroops(Soldier soldier) {
+        troops.add(soldier);
+    }
+
+    public Soldier getFrontMan() {
+        return troops.get(0);
+    }
+}
---
+++ src/test/java/be/cegeka/battle/ArmyTest.java
@@ -0,0 +1,44 @@
+package be.cegeka.battle;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by christom on 5/05/2017.
+ */
+public class ArmyTest {
+
+    Army army;
+
+    @Before
+    public void setUp() {
+
+        army = new Army();
+    }
+
+    @Test
+    public void anArmyHasSoldiers() throws Exception {
+        assertThat(army.getTroops()).isNotNull();
+    }
+
+    @Test
+    public void enroleSoldierInTheArmyTest() throws Exception {
+        Soldier soldier = new Soldier("Soldier76");
+        army.addTroops(soldier);
+        assertThat(army.getTroops()).contains(soldier);
+    }
+
+    @Test
+    public void firstAddedSoldierIsFrontMan() throws Exception {
+        Soldier soldier = new Soldier("one");
+        Soldier soldier1 = new Soldier("two");
+        Soldier soldier2 = new Soldier("three");
+        army.addTroops(soldier);
+        army.addTroops(soldier1);
+        army.addTroops(soldier2);
+
+        assertThat(army.getFrontMan()).isEqualTo(soldier);
+    }
+}
